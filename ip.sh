800b1573592f7fcc97a0696d9119d120d7b84ddc
charlotteblassel <charlotte.blassel@hetic.net>
2023-02-01 15:13:28 +0100

    pinger

---
+++ ip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ip=10.93.160.209
+for i in {1..255}
+ do
+ ping -c 1 "$ip.$i" > /dev/null
+  if [ $? -eq 0 ];
+   then echo $ip.$i >> online.txt
+  else
+   echo $ip.$i >> offline.txt
+  fi
+done
